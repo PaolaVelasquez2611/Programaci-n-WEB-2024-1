939dcc084fb6f42dfce2e1a430bcfe08641eccb3
Paola Andrea Velasquez <paovelasquezd2002@gmail.com>
2024-02-05 18:24:51 -0500

    movies map

--- Disney+/src/components/Body/Body.jsx
+++ Disney+/src/components/Body/Body.jsx
@@ -4,25 +4,74 @@ import { Movies } from './Movies/moviegroup'
 import { Studios } from './Studios/studios'
 
 
+
+
+
 export function Body() {
+
+    const movies = [
+        {
+            id: crypto.randomUUID(),
+            url: "https://cdn.milenio.com/uploads/media/2021/04/27/percy-jackson-interpretado-logan-lerman.png" 
+        
+        },
+        {
+            id: crypto.randomUUID(),
+            url: "https://www.comingsoon.net/wp-content/uploads/sites/3/2023/06/The-Little-Mermaid.jpeg",
+        
+        },
+        {
+            id: crypto.randomUUID(),
+            url: "https://wallpapercave.com/wp/wp12278885.jpg",
+        
+        },
+        {
+            id: crypto.randomUUID(),
+            url:"https://hoodzpahdesign.com/wp-content/uploads/2021/03/Luca-Movie-Title-Treatment-Unchosen-Concepts-by-Hoodzpah-for-Disney-Pixar-01-1.jpg",
+        
+        },
+        {
+            id: crypto.randomUUID(),
+            url: "https://gargoyle.uni.illinois.edu/wp-content/uploads/2016/12/moana-900x405.jpeg",
+        
+        },
+        {
+            id: crypto.randomUUID(),
+            url:"https://lcmbearfacts.com/wp-content/uploads/2019/12/0-900x506.jpg",
+        
+        },
+        {
+            id: crypto.randomUUID(),
+            url: "https://pbs.twimg.com/media/EG2CvSUUcAAsqpP.jpg:large",
+        
+        },
+        {
+            id: crypto.randomUUID(),
+            url: "https://i.insider.com/5342c716eab8eabc3b6d57c4?width=1000&format=jpeg&auto=webp",
+        
+        },
+        {
+            id: crypto.randomUUID(),
+            url: "https://static1.squarespace.com/static/6074cb7375d88d420848825c/6074d2586fb0f64b8e67121d/62290b7590b6af03e381736a/1647036883085/Turning+Red+Pixar+Movie+Poster+Horizontal.jpeg?format=1500w",
+        
+        },
+        {
+            id: crypto.randomUUID(),
+            url: "https://www.thebanner.org/sites/default/files/styles/article_detail_header/public/2022-09/Lightyear_movie.jpg?itok=tJiIf7Of",
+        
+        }
+
+
+    ]
+
+
+    
     return (
         <main id="main">
             <Banner></Banner>
             <Studios></Studios>
             <Movies title ="Action and Adventure" 
-            url="https://cdn.milenio.com/uploads/media/2021/04/27/percy-jackson-interpretado-logan-lerman.png" 
-            url2="https://www.comingsoon.net/wp-content/uploads/sites/3/2023/06/The-Little-Mermaid.jpeg"
-            url3="https://wallpapercave.com/wp/wp12278885.jpg"
-            url4="https://hoodzpahdesign.com/wp-content/uploads/2021/03/Luca-Movie-Title-Treatment-Unchosen-Concepts-by-Hoodzpah-for-Disney-Pixar-01-1.jpg"
-            url5="https://gargoyle.uni.illinois.edu/wp-content/uploads/2016/12/moana-900x405.jpeg"
-            url6="https://lcmbearfacts.com/wp-content/uploads/2019/12/0-900x506.jpg"
-            url7="https://pbs.twimg.com/media/EG2CvSUUcAAsqpP.jpg:large"
-            url8="https://i.insider.com/5342c716eab8eabc3b6d57c4?width=1000&format=jpeg&auto=webp"
-            url9="https://static1.squarespace.com/static/6074cb7375d88d420848825c/6074d2586fb0f64b8e67121d/62290b7590b6af03e381736a/1647036883085/Turning+Red+Pixar+Movie+Poster+Horizontal.jpeg?format=1500w"
-            url10="https://www.thebanner.org/sites/default/files/styles/article_detail_header/public/2022-09/Lightyear_movie.jpg?itok=tJiIf7Of"
-            
-            
-            
+            movies={movies}
             
             >
 
--- Disney+/src/components/Body/Movies/moviegroup.jsx
+++ Disney+/src/components/Body/Movies/moviegroup.jsx
@@ -3,24 +3,17 @@ import { Movie } from './movie'
 
 
 export function Movies(props) {
-    const {title, url, url2, url3, url4, url5,url6, url7, url8, url9, url10} = props
+    const {title, movies} = props
     return (
 <>
         <h3>{title}</h3>
         
         <div id="movieboxgroup">
-
-        <Movie url={url} />
-        <Movie url={url2} />
-        <Movie url={url3} />
-        <Movie url={url4} />
-        <Movie url={url5} />
-        <Movie url={url6} />
-        <Movie url={url7} />
-        <Movie url={url8} />
-        <Movie url={url9} />
-        <Movie url={url10} />
-        
+        {
+            movies.map((movie)=> {
+                return  <Movie url={movie.url} />
+            })
+        }
         </div>
         
         </>
