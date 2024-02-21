f795b823a15485f7e53665b2c01e0c182d1d7c73
Paola Andrea Velasquez <paovelasquezd2002@gmail.com>
2024-02-21 17:02:47 -0500

    type fix and sounds

--- Cownter-App/src/components/Body/Body.jsx
+++ Cownter-App/src/components/Body/Body.jsx
@@ -1,39 +1,46 @@
 import { useState } from "react"
 import { Button } from "./Buttons/Button"
 import { Counter } from "./Counter/Counter"
+import addSound from '../../assets/cow-sound-add.mp3'
+import subtractSound from '../../assets/cow-sound-sub.mp3'
+
 
 
 
 export const Body = () => {
 
     const [currentCounter, setCurrentCounter] = useState(0)
+    const audioAdd = new Audio(addSound);
+    const audioSubtract = new Audio(subtractSound);
+  
 
-    const addCounter = (type) => {
-        setCurrentCounter(currentCounter + 1)
-    }
+    const changeCounter = (type) => {
 
-    const substractCounter = (type) => {
+        if (type === "add") {
+            setCurrentCounter(currentCounter + 1)
+            audioAdd.play();
 
-        if (currentCounter > 0) {
+
+        } else if (type === "substract" && currentCounter > 0) {
             setCurrentCounter(currentCounter - 1)
-        } else {
-            console.error("Cannot subtract anymore cows!");
+            audioSubtract.play();
+
+        } else if (type === "reset") {
+            setCurrentCounter(0)
+
         }
-    }
-    const resetCounter = (type) => {
-        setCurrentCounter(0)
     }
 
     return (
         <>
-            <Counter countNumber = {currentCounter}>  </Counter>
+            <Counter countNumber={currentCounter}>  </Counter>
 
             <div className="buttons-div">
-                <Button title="-" type="substract" onClick={substractCounter}></Button>
-                <Button title="+" type="add" onClick={addCounter}></Button>
+                <Button title="-" type="substract" onClick={changeCounter}></Button>
+                <Button title="+" type="add" onClick={changeCounter}></Button>
             </div>
 
-            <Button title="Reset" type="reset" onClick={resetCounter}></Button>
+            <Button title="Reset" type="reset" onClick={changeCounter}></Button>
         </>
 
     )
