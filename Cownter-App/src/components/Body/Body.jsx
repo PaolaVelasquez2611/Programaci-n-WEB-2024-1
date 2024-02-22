e33b53ab5b63acfad091b49a27b7d37ba22da664
Paola Andrea Velasquez <paovelasquezd2002@gmail.com>
2024-02-22 18:46:50 -0500

    cow images

--- Cownter-App/src/components/Body/Body.jsx
+++ Cownter-App/src/components/Body/Body.jsx
@@ -3,45 +3,50 @@ import { Button } from "./Buttons/Button"
 import { Counter } from "./Counter/Counter"
 import addSound from '../../assets/cow-sound-add.mp3'
 import subtractSound from '../../assets/cow-sound-sub.mp3'
-
-
-
+import cowImage from '../../assets/poster.png';
 
 export const Body = () => {
 
     const [currentCounter, setCurrentCounter] = useState(0)
+    const [cowImages, setCowImages] = useState(0);
     const audioAdd = new Audio(addSound);
     const audioSubtract = new Audio(subtractSound);
-  
 
     const changeCounter = (type) => {
 
         if (type === "add") {
             setCurrentCounter(currentCounter + 1)
+            setCowImages(cowImages + 1);
             audioAdd.play();
-
 
         } else if (type === "substract" && currentCounter > 0) {
             setCurrentCounter(currentCounter - 1)
+            setCowImages(cowImages - 1);
             audioSubtract.play();
 
         } else if (type === "reset") {
             setCurrentCounter(0)
+            setCowImages(0);
 
         }
     }
-
+    const renderCowImages = () => {
+        const images = [];
+        for (let i = 0; i < cowImages; i++) {
+            images.push(<img key={i} src={cowImage} alt={`Cow ${i}`} />);
+        }
+        return images;
+    };
     return (
         <>
             <Counter countNumber={currentCounter}>  </Counter>
-
+            <div className="cow-images">{renderCowImages()}</div>
             <div className="buttons-div">
                 <Button title="-" type="substract" onClick={changeCounter}></Button>
                 <Button title="+" type="add" onClick={changeCounter}></Button>
             </div>
-
             <Button title="Reset" type="reset" onClick={changeCounter}></Button>
         </>
 
-    )
-}
+    );
+};
