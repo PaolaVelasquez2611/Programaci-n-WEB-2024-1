e993d01e6af57dd42df944e329a06f44f67c88e6
AndresNei20 <andresfelipeneira2004@gmail.com>
2024-04-15 11:39:21 -0500

    fetch-img were fixed

--- Cats-facts-app/src/services/fetch-img.js
+++ Cats-facts-app/src/services/fetch-img.js
@@ -1,11 +1,10 @@
 export const getImg = async(text) =>{
     try {
-        const response = await fetch(`https://cataas.com/cat/says/${text}`)
-        const img = await response.json();
+        const img = await fetch(`https://cataas.com/cat/says/${text}?fontSize=25&fontColor=white`)
         console.log(img)
-        return img
+        return img.url
     } catch (error) {
-        console.error('There was an error getting the image', error)
-        return 'https://i0.wp.com/katzenworld.co.uk/wp-content/uploads/2019/06/funny-cat.jpeg?fit=1020%2C1020&ssl=1'
+        console.error('There was an error getting the image', error)/* 
+        return 'https://i0.wp.com/katzenworld.co.uk/wp-content/uploads/2019/06/funny-cat.jpeg?fit=1020%2C1020&ssl=1' */
     }
 }
