65b5c8068164c0040f8e058f15accb3f23399191
Paola Andrea Velasquez <paovelasquezd2002@gmail.com>
2024-04-25 17:31:09 -0500

    created input compnt

--- FP-UnityXperience/src/App.jsx
+++ FP-UnityXperience/src/App.jsx
@@ -1,5 +1,7 @@
 import { useState } from 'react'
 import './App.css'
+import { Button } from './components/Button/Button'
+import { Input } from './components/Input/Input'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -7,6 +9,8 @@ function App() {
   return (
     <>
       <h1>Hello we Are UnityXperience</h1>
+      <Button></Button>
+      <Input></Input>
     </>
   )
 }
---
+++ FP-UnityXperience/src/components/Input/Input.jsx
@@ -0,0 +1,9 @@
+
+
+import './Input.css'
+
+export const Input = (text) => {
+  return (
+    <input className="input" placeholder="Placeholder"></input>
+  )
+}
