a89107b44d980fc531638c57e06d70c0bcd7b6ca
Paola Andrea Velasquez <paovelasquezd2002@gmail.com>
2024-04-25 19:03:20 -0500

    contact us page

--- FP-UnityXperience/src/App.jsx
+++ FP-UnityXperience/src/App.jsx
@@ -2,15 +2,14 @@ import { useState } from 'react'
 import './App.css'
 import { Button } from './components/Button/Button'
 import { Input } from './components/Input/Input'
+import { Contact } from './screens/Contact/Contact'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>Hello we Are UnityXperience</h1>
-      <Button></Button>
-      <Input></Input>
+      <Contact></Contact>
     </>
   )
 }
