0705e898a3420e9e61fca6c5bc6151883aaccfd4
AndresNei20 <andresfelipeneira2004@gmail.com>
2024-04-28 15:31:26 -0500

    project tittle modified

--- FP-UnityXperience/src/screens/Projects/Projects.jsx
+++ FP-UnityXperience/src/screens/Projects/Projects.jsx
@@ -12,7 +12,7 @@ export const Projects = () => {
       
       <DetailedProjectCard
       img="https://img.freepik.com/vector-gratis/ilustracion-icono-vector-dibujos-animados-lindo-gato-sentado-concepto-icono-naturaleza-animal-aislado-premium-vector-estilo-dibujos-animados-plana_138676-4148.jpg?size=338&ext=jpg&ga=GA1.1.672697106.1714176000&semt=sph"
-      title="Love Cats"
+      title="Love Cats 2"
       tags="ProCreate"
       description="This is a marketing project for the guacamole brand Bego, this was an academic exercise"
       owner="Juan Esteban Ramirez Perdomo & Ana Sofia Henao"/>
