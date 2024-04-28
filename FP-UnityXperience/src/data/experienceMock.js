5f1fd7fc50a28ab17170d920f9b14f8373559ca0
AndresNei20 <andresfelipeneira2004@gmail.com>
2024-04-28 18:28:53 -0500

    ExperienceLine and experienceMock created

---
+++ FP-UnityXperience/src/data/experienceMock.js
@@ -0,0 +1,23 @@
+export const experienceMock = [
+    {
+        id: crypto.randomUUID(),
+        job: "Web UI developer",
+        company: "Venturit",
+        time: "January 2022 - December 2022",
+        description: "In charge to develop the Frontend interfaces to different webs using a lot of technologias and putting them together with the Backend team"
+    },
+    {
+        id: crypto.randomUUID(),
+        job: "Web UI developer",
+        company: "Venturit",
+        time: "January 2022 - December 2022",
+        description: "In charge to develop the Frontend interfaces to different webs using a lot of technologias and putting them together with the Backend team"
+    },
+    {
+        id: crypto.randomUUID(),
+        job: "Web UI developer",
+        company: "Venturit",
+        time: "January 2022 - December 2022",
+        description: "In charge to develop the Frontend interfaces to different webs using a lot of technologias and putting them together with the Backend team"
+    },
+]
--- FP-UnityXperience/src/screens/Profile/Profile.jsx
+++ FP-UnityXperience/src/screens/Profile/Profile.jsx
@@ -1,5 +1,10 @@
+import { ExperienceLine } from "../../components/ExperienceLine/ExperienceLine"
+
 export const Profile = () => {
   return (
+    <>
     <p>Hello! i'm Profile screen</p>
+    <ExperienceLine />
+    </>
   )
 }
