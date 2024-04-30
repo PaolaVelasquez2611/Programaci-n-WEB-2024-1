535249b2604c22d07a4c17cd046e4b32f3f84c81
Santiago <santipr2004@gmail.com>
2024-04-30 11:12:25 -0500

    Feat: Services screen mobile finish

--- FP-UnityXperience/src/screens/Services/Services.jsx
+++ FP-UnityXperience/src/screens/Services/Services.jsx
@@ -1,10 +1,11 @@
+import './Services.css'
 import { Footer, Header, ServicesCards} from "../../components"
 
 export const Services = () => {
   return (
     <>
     <Header/>
-    <h1>UniteXperience Services</h1>
+    <h1 className="title-screen">UniteXperience Services</h1>
     <ServicesCards/>
     <Footer/>
     </>
