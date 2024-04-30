60da13a657bab311b599c11957f362194572a136
Jramirezzz <juanestebanrp123@gmail.com>
2024-04-30 12:05:02 -0500

    profile

---
+++ FP-UnityXperience/src/components/ProfileCard/ProfileCard.jsx
@@ -0,0 +1,35 @@
+import './ProfileCard.css'
+
+export function ProfileCard(props) {
+    const { text, imagen, title } = props;
+    return (
+        <section className='card-profile mx-auto'>
+            <div className="profile-card">
+                <img className="img-profile" src={imagen} alt="Profile Image" />
+            </div>
+            <div className='card-info'>
+                <h2 className='name'>{title}</h2>
+                <p className='descript'>{text}</p>
+
+                <section className='buttons-social-card'>
+                    <button className='social-btn-card'>
+                        <img src="../../src/assets/github.png" alt="" />
+                    </button>
+                    <button className='social-btn-card'>
+                        <img src="../../src/assets/instagram.png" alt="" />
+                    </button>
+                    <button className='social-btn-card'>
+                        <img src="../../src/assets/behance.png" alt="" />
+                    </button>
+                    <button className='social-btn-card'>
+                        <img src="../../src/assets/linkedin.png" alt="" />
+                    </button>
+                </section>
+            </div>
+
+            
+             
+           
+        </section>
+    )
+}
--- FP-UnityXperience/src/screens/Profile/Profile.jsx
+++ FP-UnityXperience/src/screens/Profile/Profile.jsx
@@ -1,11 +1,22 @@
-import { ExperienceLine,Header,Footer } from "../../components"
+import { ExperienceLine,Header,Footer,ProfileCard } from "../../components"
 
 export const Profile = () => {
   return (
     <>
-    <p>Hello! i'm Profile screen</p>
     <Header/>
+    <h1>UniteXperience Profile</h1>
+    <ProfileCard  imagen="../../src/assets/Profile/JuanRamirez.png" 
+                  title="Juan Esteban Ramirez Perdomo"
+                  text="I am a web development enthusiast with a profound interest
+                        in User Interface (UI) design. My passion for creating attractive and 
+                        functional digital experiences merges with my penchant for staying 
+                        updated with the constant technological advancements in the fields of
+                        web development and UI design."
+    />
+    <h2>My Experience</h2>
     <ExperienceLine />
+    <h2>My projects</h2>
+    
     <Footer/>
     </>
   )
