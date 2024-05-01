bc2fe101469e2d5e7d949e6df8fdf3e269e08428
Santiago Perez <santipr2004@gmail.com>
2024-04-30 21:00:55 -0500

    Feat: services screen finish

--- FP-UnityXperience/src/screens/Services/Services.jsx
+++ FP-UnityXperience/src/screens/Services/Services.jsx
@@ -1,5 +1,5 @@
 import './Services.css'
-import { Footer, Header, ServicesCards} from "../../components"
+import { Footer, Header, ServicesCards, Button} from "../../components"
 
 export const Services = () => {
   return (
@@ -7,6 +7,13 @@ export const Services = () => {
     <Header/>
     <h1 className="title-screen">UniteXperience Services</h1>
     <ServicesCards/>
+    <section className="more-aboutus">
+      <span className="combined-text">
+        <p className="blue-text">Interested?</p> 
+        <p>Contact us and bring your idea to make it a reality</p>
+      </span>
+      <Button className="learn-more" text="Learn more about us"/>
+    </section>
     <Footer/>
     </>
   )
