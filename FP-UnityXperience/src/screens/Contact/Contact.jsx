d2f6e5ec6439945e39ff5fa2c0eecf3f82a638ac
Paola Andrea Velasquez <paovelasquezd2002@gmail.com>
2024-04-30 20:01:59 -0500

    testing merge

--- FP-UnityXperience/src/screens/Contact/Contact.jsx
+++ FP-UnityXperience/src/screens/Contact/Contact.jsx
@@ -13,7 +13,7 @@ export const Contact = () => {
     <Input text="Your Name or Company" placeholder="Enter name"></Input>
     <Input text="Your Email" placeholder="Enter email"></Input>
     <Input text="Something you want to Say?" className="tall-input" placeholder="Write it here"></Input>
-    <Button></Button>
+    <Button text={"Send"}></Button>
     </div>
    </div>
   )
