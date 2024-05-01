92ff56d72998e3bcc3c8485945db68b2e24db326
Paola Andrea Velasquez <paovelasquezd2002@gmail.com>
2024-05-01 16:41:46 -0500

    fixed problem with css in cardwrap

--- FP-UnityXperience/src/components/CardWrap/CardWrap.jsx
+++ FP-UnityXperience/src/components/CardWrap/CardWrap.jsx
@@ -5,8 +5,12 @@ export const CardWrap = (props) => {
     return (
 
    <div className='card-wrap'>
+    <div className='image-container'>
     <img src={image} />
+    </div>
+    <div className='children-container'>
     {children}
+    </div>
     </div>
   )
 }
