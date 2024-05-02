8bf6b89b41db169b93ba3a0e64818790eb4d1c61
Santiago Perez <santipr2004@gmail.com>
2024-05-02 07:26:47 -0500

    Feat: corrections in the projects container

--- FP-UnityXperience/src/screens/Projects/Projects.jsx
+++ FP-UnityXperience/src/screens/Projects/Projects.jsx
@@ -1,7 +1,7 @@
 import './Projects.css'
 import { useState, useEffect } from 'react'
 import { projects } from '../../data/projects'
-import { Footer, Header, DetailedProjectCard, ProjectCards } from "../../components"
+import { Footer, Header, ProjectCards } from "../../components"
 
 export const Projects = () => {
 
@@ -16,11 +16,10 @@ export const Projects = () => {
   const filteredProject = project.filter(project =>
     (project.title && project.title.includes(searchTerm.trim())) ||
     (project.review && project.review.toString() === searchTerm.trim()) ||
-    (project.tags && project.tags.includes(searchTerm.trim()))
+    (project.tags[0] && project.tags[0].includes(searchTerm.trim())) ||
+    (project.tags[1] && project.tags[1].includes(searchTerm.trim())) ||
+    (project.tags[2] && project.tags[2].includes(searchTerm.trim()))
     );
-
-    console.log('searchTerm:', searchTerm);
-    console.log('filteredData:', filteredProject);
 
   return (
     <>
@@ -38,14 +37,7 @@ export const Projects = () => {
 
       <h1 className="title-screen">UnityXperience Projects </h1>
       
-        <ProjectCards filteredData={filteredProject}/>
-
-        <DetailedProjectCard
-        img="https://img.freepik.com/vector-gratis/ilustracion-icono-vector-dibujos-animados-lindo-gato-sentado-concepto-icono-naturaleza-animal-aislado-premium-vector-estilo-dibujos-animados-plana_138676-4148.jpg?size=338&ext=jpg&ga=GA1.1.672697106.1714176000&semt=sph"
-        title="Love Cats 2"
-        tags="ProCreate"
-        description="This is a marketing project for the guacamole brand Bego, this was an academic exercise"
-        owner="Juan Esteban Ramirez Perdomo & Ana Sofia Henao"/>
+      <ProjectCards filteredData={filteredProject}/>
 
       <Footer/>
     </>
