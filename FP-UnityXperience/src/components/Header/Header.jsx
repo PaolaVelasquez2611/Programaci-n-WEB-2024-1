3f50a72d17a707c5a37af2dd478df9f4792437a0
Jramirezzz <juanestebanrp123@gmail.com>
2024-05-24 18:34:55 -0500

    first part ham menu

--- FP-UnityXperience/src/components/Header/Header.jsx
+++ FP-UnityXperience/src/components/Header/Header.jsx
@@ -1,39 +1,39 @@
-import { MobileNavBar,Nav} from "../../components"
 import { useState } from "react";
-import './Header.css'
+import './Header.css';
+import { MobileNavBar, Nav } from "../../components";
 import { headerData } from "../../data/headerData";
 import { Link } from "react-router-dom";
 
-export function Header (){
-    const [showMenu, setShowMenu] = useState(false)
+export function Header() {
+    const [showMenu, setShowMenu] = useState(false);
 
-    const handleClick = (event) => {
-        setShowMenu(!showMenu)
-    }
+    const handleClick = () => {
+        setShowMenu(!showMenu);
+    };
+
     return (
-        <div className={`Container ${showMenu ? "showMenu" : ""}`}>
-      <img
-        className="logo"
-        src="../../src/assets/Logo.png"
-        alt="Logo UnityXperience"
-      />
-      {headerData.map(({ id, text, route }) => (
-        <section key={id} className="Section">
-          <Link to={route}>
-            <Nav text={text} />
-          </Link>
-        </section>
-      ))}
-      <section>
-        <button className="hamClick" onClick={handleClick}>
-          <img
-            className="hamburguer-menu"
-            src="../../src/assets/SVG/hamburguer-menu.svg"
-            alt=""
-          />
-        </button>
-      </section>
-      <section>{!showMenu ? null : <MobileNavBar />}</section>
-    </div>
+        <div className="Container">
+            <img
+                className="logo"
+                src="../../src/assets/Logo.png"
+                alt="Logo UnityXperience"
+            />
+            <div className={`Nav ${showMenu ? "showMenu" : ""}`}>
+                {headerData.map(({ id, text, route }) => (
+                    <section key={id} className="Section">
+                        <Link to={route} onClick={() => setShowMenu(false)}>
+                            <Nav text={text} />
+                        </Link>
+                    </section>
+                ))}
+            </div>
+            <button className="hamClick" onClick={handleClick}>
+                <img
+                    className="hamburguer-menu"
+                    src="../../src/assets/SVG/hamburguer-menu.svg"
+                    alt=""
+                />
+            </button>
+        </div>
     );
 }
