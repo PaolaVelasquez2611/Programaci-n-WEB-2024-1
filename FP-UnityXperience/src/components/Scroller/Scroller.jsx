5dd321b3b0106850048e36f19ae547936c0b0268
Santiago Perez <santipr2004@gmail.com>
2024-05-26 22:51:36 -0500

    Feat: scroller and landing corrections with firebase

--- FP-UnityXperience/src/components/Scroller/Scroller.jsx
+++ FP-UnityXperience/src/components/Scroller/Scroller.jsx
@@ -1,40 +1,8 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { ProjectCard } from "../ProjectCard/ProjectCard";
-import { collection, query, onSnapshot, getFirestore } from "firebase/firestore";
-import { app } from '../../services/firebase-config';
 import "./Scroller.css";
 
-const db = getFirestore(app);
-
-export const Scroller = () => {
-  const [projects, setProjects] = useState([]);
-  
-  const getProjectsFromFirestore = () => {
-    const q = query(collection(db, "projects"));
-    onSnapshot(q, (querySnapshot) => {
-      const updatedProjects = [];
-      querySnapshot.forEach((doc) => {
-        updatedProjects.push(doc.data());
-      });
-
-      localStorage.setItem('projects', JSON.stringify(updatedProjects));
-    });
-  };
-
-  const getProjects = () => {
-
-    const localProjects = JSON.parse(localStorage.getItem('projects'));
-    if (localProjects && localProjects.length > 0) {
-      setProjects(localProjects);
-    }
-
-    getProjectsFromFirestore();
-  };
-
-  useEffect(() => {
-    getProjects();
-  }, []);
-
+export const Scroller = ({ projects }) => {
   useEffect(() => {
     const scrollers = document.querySelectorAll(".projects-scroll");
 
@@ -58,28 +26,22 @@ export const Scroller = () => {
     }
   }, []);
 
-  console.log(projects)
-
   return (
     <section className="max-w-screen-xl mx-auto md:w-5/6 lg:w-4/6 pt-10 flex flex-col justify-center items-center">
       <div className="projects-scroll w-full inline-flex flex-nowrap overflow-hidden" data-direction="left">
         <ul className="scroller-inner flex justify-center items-center">
-          {projects.map(( project ) => (
-            <li>
-              <ProjectCard
-                project={project}
-              />
+          {projects.map((project) => (
+            <li key={project.id}>
+              <ProjectCard project={project} />
             </li>
           ))}
         </ul>
       </div>
       <div className="projects-scroll inline-flex flex-nowrap overflow-hidden" data-direction="right">
-        <ul className="scroller-inner flex justify-center items-center ">
-          {projects.map(( project ) => (
-            <li>
-              <ProjectCard
-                project={project}
-              />
+        <ul className="scroller-inner flex justify-center items-center">
+          {projects.map((project) => (
+            <li key={project.id}>
+              <ProjectCard project={project} />
             </li>
           ))}
         </ul>
