0f9f2f00dd20339d2b390038595a8017ab4b16bc
AndresNei20 <andresfelipeneira2004@gmail.com>
2024-05-29 12:23:47 -0500

    teamInformation completed 3/4

--- FP-UnityXperience/src/data/teamInformation.js
+++ FP-UnityXperience/src/data/teamInformation.js
@@ -1,5 +1,83 @@
 export const teamInformation = [
     {
-        
-    }
+        name: "Santiago Perez Ramirez",
+        rol: "Front-end developer",
+        tools: ["JS", "React", "Firebase"],
+        social_media: {
+            linkedin: "https://www.linkedin.com/in/santiago-perez-561139221/",
+            github: "https://github.com/Santipr29",
+            instagram: "https://www.instagram.com/santi_2903/",
+            behance: "https://www.behance.net/santiagoperez37"
+        },
+        description: "I am a Front End Developer passionate about UI design. With solid experience in creating web applications, I focus on developing solutions that are not only visually appealing but also highly accessible and usable. My goal is to provide intuitive and functional digital experiences, paying special attention to details and user interaction.",
+        experience: [
+            {
+                job: "E-commerce Specialist",
+                company: "Traygo",
+                time: "June 2023 - July 2023",
+                job_description: "Responsible for managing and optimizing the MercadoLibre platform for product sales, including the creation and administration of listings, inventory management, customer service, and sales analysis to maximize product performance and visibility on the platform."
+            }
+        ]
+    },
+    {
+        name: "Juan Esteban Ramirez Perdomo",
+        rol: "Comedian",
+        tools: ["JS", "React", "Figma"],
+        social_media: {
+            linkedin: "https://www.linkedin.com/in/juan-esteban-ramírez-perdomo-55b7382a5/",
+            github: "https://github.com/Jramirezzz",
+            instagram: "https://www.instagram.com/juanestebanramirezp/?hl=es",
+            behance: "https://www.behance.net/juanesramirez5"
+        },
+        description: "I am a front-end developer passionate about creating intuitive and engaging digital experiences. My interest in user interface (UI) design and user experience (UX) drives me to build websites and applications that are not only visually impressive but also functional and easy to use. Every project I work on is guided by a deep commitment to innovation and creativity, always striving to exceed the expectations of both users and clients.",
+        experience: [
+            {
+                job: "Brand Designer (Freelance)",
+                company: "Freelance",
+                time: "June 2022 - June 2023",
+                job_description: "Freelance designer, developer and full time comedian"
+            }
+        ]
+    },
+    {
+        name: "Andrés Felipe Neira Méndez",
+        rol: "Comedian",
+        tools: ["JS", "Figma", "React", "Node"],
+        social_media: {
+            linkedin: "https://www.linkedin.com/in/andresnei/",
+            github: "https://github.com/AndresNei20",
+            instagram: "https://www.instagram.com/andresnei20/",
+            behance: "https://www.behance.net/andres_0820"
+        },
+        description: "Hey there! I'm Andres Neira, a front-end web UI developer dedicated to continuous learning and growth. My goal isn't just to be another front-end developer, but to strive towards becoming an outstanding individual, a visionary designer, and a creative thinker. I'm committed to providing my clients with top-notch solutions to their problems, ensuring they are scalable, accessible, visually appealing, and, most importantly, impactful. Let's collaborate and create something remarkable together! ",
+        experience: [
+            {
+                job: "Brand Designer (Freelance)",
+                company: "Freelance",
+                time: "June 2022 - June 2023",
+                job_description: "Freelance designer, developer and full time comedian"
+            }
+        ]
+    },
+    {
+        name: "Paola Andrea Velasquez Durán",
+        rol: "UI/UX Designer", 
+        tools: ["PRocreate", "Figma"], /* maybe would change */
+        social_media: {
+            linkedin: "https://www.linkedin.com/feed/",
+            github: "https://github.com/PaolaVelasquez2611",
+            instagram: "https://www.instagram.com/paola_andrea1320/",
+            behance: "https://www.behance.net/paolavelasquez4"
+        },
+        description: "Pao descripton",
+        experience: [
+            {
+                job: "pao jo",
+                company: "pao company",
+                time: "time - timetime",
+                job_description: "pao job description"
+            }
+        ]
+    },
+
 ]
