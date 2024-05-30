28434a1606324360a771f5fd4f3239cb26329918
Jramirezzz <juanestebanrp123@gmail.com>
2024-05-29 20:44:03 -0500

    buttons link

--- FP-UnityXperience/src/components/ProfileCard/ProfileCard.jsx
+++ FP-UnityXperience/src/components/ProfileCard/ProfileCard.jsx
@@ -1,36 +1,30 @@
-import './ProfileCard.css'
+import './ProfileCard.css';
 
-export function ProfileCard({ text, imagen, title }) {
+export function ProfileCard({ text, imagen, title, socialMedia }) {
     return (
-        <section className='card-profile '>
+        <section className='card-profile'>
             <div className="profile-card">
-                <img className="img-profile" 
-                     src={imagen} 
-                     alt="Profile Image" />
+                <img className="img-profile" src={imagen} alt="Profile Image" />
             </div>
             <div className='card-info'>
                 <h2 className='name'>{title}</h2>
                 <p className='descript'>{text}</p>
 
                 <section className='buttons-social-card'>
-                    <button className='social-btn-card'>
-                        <img src="../../src/assets/github.png"  
-                             alt="" />
-                    </button>
-                    <button className='social-btn-card'>
-                        <img src="../../src/assets/instagram.png" 
-                             alt="" />
-                    </button>
-                    <button className='social-btn-card'>
-                        <img src="../../src/assets/behance.png" 
-                            alt="" />
-                    </button>
-                    <button className='social-btn-card'>
-                        <img src="../../src/assets/linkedin.png" 
-                             alt="" />
-                    </button>
+                    <a className='social-btn-card' href={socialMedia.github} target="_blank" rel="noopener noreferrer">
+                        <img src="../../src/assets/github.png" alt="GitHub" />
+                    </a>
+                    <a className='social-btn-card' href={socialMedia.instagram} target="_blank" rel="noopener noreferrer">
+                        <img src="../../src/assets/instagram.png" alt="Instagram" />
+                    </a>
+                    <a className='social-btn-card' href={socialMedia.behance} target="_blank" rel="noopener noreferrer">
+                        <img src="../../src/assets/behance.png" alt="Behance" />
+                    </a>
+                    <a className='social-btn-card' href={socialMedia.linkedin} target="_blank" rel="noopener noreferrer">
+                        <img src="../../src/assets/linkedin.png" alt="LinkedIn" />
+                    </a>
                 </section>
             </div>
         </section>
-    )
+    );
 }
